5e0f9413fe176cb0d306dc9a940aeb9a3dcd0060
plantsoup <seed@spearg.com>
2025-10-05 21:04:46 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@
 // This API key is for demonstration purposes.
 // In a production environment, this should be replaced with an environment variable.
 export const API_KEY = '';
-export const API_URL = 'https://fxworker.gav-c8d.workers.dev';
+export const API_URL = 'https://fxworker.gav-c8d.workers.dev/latest/';
